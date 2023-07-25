5661747b62db14ce3597a12cc2092150201017a4
lfk <lf.kre@t-online.de>
2023-07-25 20:51:53 +0200

    fixing DropDown color

--- src/pages/api/components/DropDown.tsx
+++ src/pages/api/components/DropDown.tsx
@@ -1,11 +1,24 @@
 export default function DropDown(props: DropDownProps) {
-      console.log(props.color);
-
-      return (
-            <div className={`bg-${props.color} rounded-md p-0.5 w-[160px]`}>
-                  <h1 className="text-3xl text-center">{props.text}</h1>
-            </div>
-      );
+      switch (props.color) {
+            case "orange":
+                  return (
+                        <div className={`bg-orange rounded-md p-0.5 w-[160px]`}>
+                              <h1 className="text-3xl text-center">{props.text}</h1>
+                        </div>
+                  );
+            case "purple":
+                  return (
+                        <div className={`bg-purple rounded-md p-0.5 w-[160px]`}>
+                              <h1 className="text-3xl text-center">{props.text}</h1>
+                        </div>
+                  );
+            default:
+                  return (
+                        <div className={`bg-black rounded-md p-0.5 w-[160px]`}>
+                              <h1 className="text-3xl text-center">{props.text}</h1>
+                        </div>
+                  );
+      }
 }
 
 type DropDownProps = {
--- src/pages/api/components/Item.tsx
+++ src/pages/api/components/Item.tsx
@@ -2,7 +2,7 @@ import DropDown from "./DropDown";
 
 export default function Item(props: ItemProps) {
       return (
-            <div className="bg-background border-2 border-border rounded-md m-1 flex justify-between items-center px-1 py-0.5">
+            <div className="bg-background border-2 border-border rounded-md m-1 flex flex-col justify-between items-center px-1 py-0.5 sm:flex-row">
                   <h1 className="text-black w-24 text-3xl">{props.item.name}</h1>
                   <DropDown color={"orange"} text={props.item.hall}></DropDown>
                   <DropDown color={"purple"} text={`Ebene ${props.item.layer}-${props.item.index}`}></DropDown>
